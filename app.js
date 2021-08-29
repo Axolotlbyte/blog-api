e38318cacfc75de42a36fb3acdeca9123e3d29e5
Arzaan <arjaankhan@gmail.com>
2021-08-29 17:58:46 +0530

    api additions

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var postRouter = require('./routes/post');
+var signupRouter = require('./routes/signup');
 
 var app = express();
 var mongoose = require('mongoose');
@@ -18,8 +19,9 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(express.json({ extended: true }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,8 +29,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/post', postRouter);
+app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,7 +46,12 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({
+    message: err.message,
+    error: err
+  });
+  return;
 });
 
 module.exports = app;
+   
