7a07de4e9df387165a8e9d88c9367d9a97712054
Arzaan <arjaankhan@gmail.com>
2021-08-31 17:38:46 +0530

    api login

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/login', loginRouter);
+app.use('/', loginRouter);
 app.use('/post', postRouter);
 app.use('/signup', signupRouter);
 
